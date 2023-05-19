9f41c111030e81e97c0a8c8617f681d5870c24dd
BryanPineda21 <109675212+BryanPineda21@users.noreply.github.com>
2023-05-19 17:17:54 -0400

    1 file modified, 1 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,45 +1,41 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 export const App = () => {
-  const [count,setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-  const onIncrementClick = () =>{
-    setCount(count + 1)
-  }
-  const onDecrementClick = () =>{
-    setCount(count - 1)
-  }
+  const onIncrementClick = () => {
+    setCount(count + 1);
+  };
+  const onDecrementClick = () => {
+    setCount(count - 1);
+  };
 
   return (
     <div className="container">
       <h1>code-next-react-counter</h1>
 
-      
-
       <hr />
 
       <div>
         <h1>Count: {count}</h1>
-        <button 
-        id = "increment-btn"
-        className = "btn btn-success"  onClick ={onIncrementClick}
-        style = {{ marginRight : '8px'}}
-        >increment
+        <button
+          id="increment-btn"
+          className="btn btn-success"
+          onClick={onIncrementClick}
+          style={{ marginRight: '8px' }}
+        >
+          increment
         </button>
-        
-        <button 
-        id = "decrement-btn"
-        className = "btn btn-warning" onClick ={onDecrementClick}>
+
+        <button
+          id="decrement-btn"
+          className="btn btn-warning"
+          onClick={onDecrementClick}
+        >
           decrement
         </button>
-        
-
       </div>
     </div>
   );
 };
-
-
-
-
